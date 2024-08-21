8191a6ed76f96f53d263171d784ae31522d9c24e
Yagho Petini <yprogy@gmail.com>
2024-08-21 16:54:05 -0300

    .

--- collectables/Coin/coin_collectable.gd
+++ collectables/Coin/coin_collectable.gd
@@ -1,20 +1,13 @@
 extends BaseCollectable
 
-@export_category("Variables")
-@export var _texture: AnimatedSprite2D
-
 func _on_body_entered(_body) -> void:
 	if _body is BaseCharacter:
 		_consume(_body)
 		
-
 func _consume(_body) -> void:
 	_texture.play("collect")
+	_song.play()
 	global.score += 100
-	print(global.score)
-	
-	#pensar em colocar efeito após a coleta.
-
 
 func _on_texture_animation_finished():
 	if _texture.animation == "collect":
--- collectables/base_collectable.gd
+++ collectables/base_collectable.gd
@@ -1,6 +1,11 @@
 extends Area2D
 class_name BaseCollectable
 
+@export_category("Object")
+@export var _texture: AnimatedSprite2D
+@export var _song: AudioStreamPlayer
+
+
 func _on_body_entered(_body) -> void:
 	pass
 		
